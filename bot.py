ec6a987b275097893e8f715ab59f75839ea9a005
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-01-24 19:44:11 +0100

    More features

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import re
 import discord
-from random import random
+from random import random, randint
 
 TOKEN = "ENTER YOUR DISCORD TOKEN HERE"
 client = discord.Client()
@@ -8,33 +8,53 @@ client = discord.Client()
 linux_copy_pasta = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
 
 # Non case sensitive, keys must be words
-word_responses = {"slapen": "Slaapwel!",
-                  "kat": "Miauw",
-                  "hond": "Woef",
-                  "haan": "Kukeleku",
-                  "vis": "Blub",
-                  "linux": linux_copy_pasta}
+responses = {"slapen": "Slaapwel!",
+             "kat": "Miauw",
+             "hond": "Woef",
+             "haan": "Kukeleku",
+             "vis": "Blub",
+             "linux": linux_copy_pasta,
+             "slok of gene slok": "WINOK"}
 # Multiple keys with same value
-word_responses.update(dict.fromkeys(
-    ['eten', 'lunchen', 'dinner', 'voedsel', 'lunch', 'ontbijt', 'breakfast', 'brunch', 'snack', 'snacken'],
+responses.update(dict.fromkeys(
+    ['eten', 'lunchen', 'dinner', 'voedsel', 'lunch', 'ontbijt', 'breakfast', 'brunch', 'snack', 'snacken', 'eeten'],
     "Smakelijk!"))
-word_responses.update(dict.fromkeys(['vos', 'fox'], "Ring-ding-ding-ding-dingeringeding!"))
-word_responses.update(dict.fromkeys(['aescu', 'aesculapia'], "ieuw"))
+responses.update(dict.fromkeys(['vos', 'fox'], "Ring-ding-ding-ding-dingeringeding!"))
+responses.update(dict.fromkeys(['aescu', 'aesculapia'], "ieuw"))
 
-phrase_responses = {"slok of gene slok": "WINOK"}
+emojis = {",p leave": "👋",
+          "good bot": "😘",
+          "bad bot": "😠",
+          'hou van': "😍",
+          "succes": "❤",
+          "tea": "🍵",
+          "cupcake": "🧁",
+          "lockdown": "🔒⬇",
+          "se": "🤮",
+          'bollekebot': "👀",
+          "weyts": "💩",
+          "bolleke": "🟠"
+          }
+emojis.update(dict.fromkeys(['birthday', "verjaardag"], "🥳"))
+emojis.update(dict.fromkeys(['love', 'liefde'], "😍"))
+emojis.update(dict.fromkeys(['dead', 'dood'], "🔫"))
+emojis.update(dict.fromkeys(['corona', 'covid'], "🤧"))
+emojis.update(dict.fromkeys(['king', 'mking', 'deceuninck'], "👑"))
 
-emoji_reactions = {",p leave": "👋",
-                   "succes": "❤",
-                   "you have been unsubscribed due to inactivity!": "😢",
-                   "dood": "🔫",
-                   "Bollekebot": "👀",
-                   "tea": "🍵",
-                   "cupcake": "🧁",
-                   "good bot": "😘",
-                   "bad bot": "😠"}
-emoji_reactions.update(dict.fromkeys(['SE', 'Software Engineering'], "🤮"))
-emoji_reactions.update(dict.fromkeys(['birthday', "Birthday", "verjaardag", 'Verjaardag'], "🥳"))
-emoji_reactions.update(dict.fromkeys(['love', 'liefde', 'hou van'], "😍"))
+
+def split_words_phrases(dictionary):
+    words = dict()
+    phrases = dict()
+    for key in dictionary:
+        if ' ' in key:
+            phrases[key] = dictionary[key]
+        else:
+            words[key] = dictionary[key]
+    return words, phrases
+
+
+word_responses, phrase_responses = split_words_phrases(responses)
+emoji_reactions, emoji_reactions_phrases = split_words_phrases(emojis)
 
 
 async def add_reactions(message, reactions):
@@ -47,6 +67,8 @@ async def on_ready():
     print(f'Active on: ')
     for guild in client.guilds:
         print(f'- {guild.name}')
+    await client.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name="Mateo climb up the leaderboard"))
 
 
 @client.event
@@ -62,12 +84,17 @@ async def on_message(message):
             message.author == client.user:
         return
 
+    if message.author.bot:
+        if "you have been unsubscribed due to inactivity!" in message.content:
+            await message.add_reaction("😢")
+        return
+
     # Get the lowercase message without special symbol
     lower_message = message.content.lower().replace(',', '').replace('!', '').replace('?', '').replace('.', '')
     # Get the words (split the spaces), a set is faster to search in
     words = set(lower_message.split())
 
-    global phrase_responses, emoji_reactions, word_responses
+    global phrase_responses, emoji_reactions, word_responses, emoji_reactions_phrases
 
     # Phrase responses
     for key in phrase_responses:
@@ -78,11 +105,13 @@ async def on_message(message):
     for word in words:
         if word in word_responses:
             await message.reply(word_responses[word])
+        if word in emoji_reactions:
+            await add_reactions(message, emoji_reactions[word])
 
     # emoji reactions
-    for key in emoji_reactions:
+    for key in emoji_reactions_phrases:
         if key in message.content:
-            await add_reactions(message, emoji_reactions[key])
+            await add_reactions(message, emoji_reactions_phrases[key])
             # await message.add_reaction(emoji_reactions[key])
 
     # Special cases
@@ -91,15 +120,22 @@ async def on_message(message):
     if pattern := re.match("[iI]k ben (.*)", message.content):
         await message.reply(f"Hey {pattern.group(1)}, ik ben Bollekebot!")
     if message.author.display_name == "Alexander" and random() < 0.1:
-            await message.add_reaction("🥴")
+        await message.add_reaction("🥴")
     if "69" in message.content and not message.author.bot:
         await add_reactions(message, "🇳🇮🇨🇪")
     if "mateo" in lower_message and message.guild.name == "WINAK" and not message.author.bot:
         await message.add_reaction(discord.utils.get(message.guild.emojis, name="mateo"))
+    if "adios" in lower_message and message.guild.name == "WINAK" and not message.author.bot:
+        await message.add_reaction(discord.utils.get(message.guild.emojis, name="zateo"))
+    if "punten" in lower_message:
+        await message.reply(f"Je hebt {randint(0, 20)}/20")
+
 
 @client.event
 async def on_reaction_add(reaction, user):
-    if reaction.message.content == "try me":
+    if not user.bot and reaction.message.content == "try me":
         await reaction.remove(user)
+        await reaction.message.add_reaction(reaction.emoji)
+
 
 client.run(TOKEN)
