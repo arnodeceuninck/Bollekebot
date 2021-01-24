82add876429596e45c405db16a6fab564f2063ca
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-01-24 22:16:03 +0100

    say feature

--- bot.py
+++ bot.py
@@ -14,10 +14,13 @@ responses = {"slapen": "Slaapwel!",
              "haan": "Kukeleku",
              "vis": "Blub",
              "linux": linux_copy_pasta,
-             "slok of gene slok": "WINOK"}
+             "slok of gene slok": "WINOK",
+             "water": "WATER?! Wil je me soms vergiftigen ofzo?",
+             "anne-marie": "*Anne Marie",
+             }
 # Multiple keys with same value
 responses.update(dict.fromkeys(
-    ['eten', 'lunchen', 'dinner', 'voedsel', 'lunch', 'ontbijt', 'breakfast', 'brunch', 'snack', 'snacken', 'eeten'],
+    ['eten', 'lunchen', 'dinner', 'voedsel', 'lunch', 'ontbijt', 'breakfast', 'brunch', 'snack', 'snacken', 'eeten', 'ete'],
     "Smakelijk!"))
 responses.update(dict.fromkeys(['vos', 'fox'], "Ring-ding-ding-ding-dingeringeding!"))
 responses.update(dict.fromkeys(['aescu', 'aesculapia'], "ieuw"))
@@ -25,21 +28,28 @@ responses.update(dict.fromkeys(['aescu', 'aesculapia'], "ieuw"))
 emojis = {",p leave": "👋",
           "good bot": "😘",
           "bad bot": "😠",
-          'hou van': "😍",
-          "succes": "❤",
           "tea": "🍵",
           "cupcake": "🧁",
           "lockdown": "🔒⬇",
           "se": "🤮",
           'bollekebot': "👀",
           "weyts": "💩",
-          "bolleke": "🟠"
+          "bolleke": "🟠",
+          "sad": "😢",
+          "cara": "🍻",
+          "anne marie": "❤",
+          "dorien": "🍻",
+          "gitte": "💜",
+          "alexander": "🥴"
           }
+emojis.update(dict.fromkeys(['pussy', '50 shades'], "😏"))
 emojis.update(dict.fromkeys(['birthday', "verjaardag"], "🥳"))
-emojis.update(dict.fromkeys(['love', 'liefde'], "😍"))
-emojis.update(dict.fromkeys(['dead', 'dood'], "🔫"))
+emojis.update(dict.fromkeys(['love', 'liefde', 'hou van'], "😍"))
+emojis.update(dict.fromkeys(['dead', 'dood', 'die'], "🔫"))
 emojis.update(dict.fromkeys(['corona', 'covid'], "🤧"))
-emojis.update(dict.fromkeys(['king', 'mking', 'deceuninck'], "👑"))
+emojis.update(dict.fromkeys(['king', 'mking', 'deceuninck', 'koning'], "👑"))
+emojis.update(dict.fromkeys(['sneeuw', 'sneeuwman'], "☃"))
+emojis.update(dict.fromkeys(["succes", "success"], "❤"))
 
 
 def split_words_phrases(dictionary):
@@ -70,12 +80,20 @@ async def on_ready():
     await client.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name="Mateo climb up the leaderboard"))
 
+def multi_replace(string, replace):
+    for char in replace:
+        string = string.replace(char, '')
+    return string
 
 @client.event
 async def on_message(message):
     # Whoever you are, wherever you are, never say WINAK with lowercase letters
     if "winak" in message.content:
         await message.reply("*WINAK")
+
+    if pattern := re.match("-say (.*)", message.content):
+        await message.delete()
+        await message.channel.send(pattern.group(1))
 
     # Don't react to my own messages (except in my own bot test channel), only react to messages in the Pomodoro channel
     # and don't react to messages from this bot
@@ -90,7 +108,7 @@ async def on_message(message):
         return
 
     # Get the lowercase message without special symbol
-    lower_message = message.content.lower().replace(',', '').replace('!', '').replace('?', '').replace('.', '')
+    lower_message = multi_replace(message.content.lower(), ",!?.*")
     # Get the words (split the spaces), a set is faster to search in
     words = set(lower_message.split())
 
@@ -110,7 +128,7 @@ async def on_message(message):
 
     # emoji reactions
     for key in emoji_reactions_phrases:
-        if key in message.content:
+        if key in lower_message:
             await add_reactions(message, emoji_reactions_phrases[key])
             # await message.add_reaction(emoji_reactions[key])
 
@@ -129,6 +147,8 @@ async def on_message(message):
         await message.add_reaction(discord.utils.get(message.guild.emojis, name="zateo"))
     if "punten" in lower_message:
         await message.reply(f"Je hebt {randint(0, 20)}/20")
+    if "haha" in lower_message:
+        await message.add_reaction("😆")
 
 
 @client.event
