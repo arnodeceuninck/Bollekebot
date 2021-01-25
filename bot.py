61b06ecc67c41bafe739bf2ba18deb1608820133
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-01-25 12:58:45 +0100

    refactored code

--- bot.py
+++ bot.py
@@ -1,55 +1,10 @@
 import re
 import discord
 from random import random, randint
+from config import emojis, responses
 
 TOKEN = "ENTER YOUR DISCORD TOKEN HERE"
 client = discord.Client()
-
-linux_copy_pasta = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
-
-# Non case sensitive, keys must be words
-responses = {"slapen": "Slaapwel!",
-             "kat": "Miauw",
-             "hond": "Woef",
-             "haan": "Kukeleku",
-             "vis": "Blub",
-             "linux": linux_copy_pasta,
-             "slok of gene slok": "WINOK",
-             "water": "WATER?! Wil je me soms vergiftigen ofzo?",
-             "anne-marie": "*Anne Marie",
-             }
-# Multiple keys with same value
-responses.update(dict.fromkeys(
-    ['eten', 'lunchen', 'dinner', 'voedsel', 'lunch', 'ontbijt', 'breakfast', 'brunch', 'snack', 'snacken', 'eeten', 'ete'],
-    "Smakelijk!"))
-responses.update(dict.fromkeys(['vos', 'fox'], "Ring-ding-ding-ding-dingeringeding!"))
-responses.update(dict.fromkeys(['aescu', 'aesculapia'], "ieuw"))
-
-emojis = {",p leave": "👋",
-          "good bot": "😘",
-          "bad bot": "😠",
-          "tea": "🍵",
-          "cupcake": "🧁",
-          "lockdown": "🔒⬇",
-          "se": "🤮",
-          'bollekebot': "👀",
-          "weyts": "💩",
-          "bolleke": "🟠",
-          "sad": "😢",
-          "cara": "🍻",
-          "anne marie": "❤",
-          "dorien": "🍻",
-          "gitte": "💜",
-          "alexander": "🥴"
-          }
-emojis.update(dict.fromkeys(['pussy', '50 shades'], "😏"))
-emojis.update(dict.fromkeys(['birthday', "verjaardag"], "🥳"))
-emojis.update(dict.fromkeys(['love', 'liefde', 'hou van'], "😍"))
-emojis.update(dict.fromkeys(['dead', 'dood', 'die'], "🔫"))
-emojis.update(dict.fromkeys(['corona', 'covid'], "🤧"))
-emojis.update(dict.fromkeys(['king', 'mking', 'deceuninck', 'koning'], "👑"))
-emojis.update(dict.fromkeys(['sneeuw', 'sneeuwman'], "☃"))
-emojis.update(dict.fromkeys(["succes", "success"], "❤"))
 
 
 def split_words_phrases(dictionary):
@@ -69,7 +24,10 @@ emoji_reactions, emoji_reactions_phrases = split_words_phrases(emojis)
 
 async def add_reactions(message, reactions):
     for reaction in reactions:
-        await message.add_reaction(reaction)
+        try:
+            await message.add_reaction(reaction)
+        except discord.errors.HTTPException:
+            print(f"Unknown emoji {reaction}")
 
 
 @client.event
@@ -80,10 +38,22 @@ async def on_ready():
     await client.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name="Mateo climb up the leaderboard"))
 
+
 def multi_replace(string, replace):
     for char in replace:
         string = string.replace(char, '')
     return string
+
+
+def is_me(user):
+    return user.display_name == "Arno Deceuninck"
+
+
+def should_react(message):
+    return not (message.channel.name != "pomodoro" or
+                (is_me(message.author) and message.guild.name != "Bot Tests" and not "x" in message.content) or
+                message.author == client.user)
+
 
 @client.event
 async def on_message(message):
@@ -91,48 +61,64 @@ async def on_message(message):
     if "winak" in message.content:
         await message.reply("*WINAK")
 
-    if pattern := re.match("-say (.*)", message.content):
-        await message.delete()
-        await message.channel.send(pattern.group(1))
+    if is_me(message.author):
+        await check_manual_commands(message)
 
     # Don't react to my own messages (except in my own bot test channel), only react to messages in the Pomodoro channel
     # and don't react to messages from this bot
-    if message.channel.name != "pomodoro" or \
-            (message.author.display_name == "Arno Deceuninck" and message.guild.name != "Bot Tests") or \
-            message.author == client.user:
+    if not should_react(message):
         return
 
     if message.author.bot:
-        if "you have been unsubscribed due to inactivity!" in message.content:
-            await message.add_reaction("😢")
-        return
+        return await bot_only_reactions(message)
 
     # Get the lowercase message without special symbol
     lower_message = multi_replace(message.content.lower(), ",!?.*")
     # Get the words (split the spaces), a set is faster to search in
     words = set(lower_message.split())
 
-    global phrase_responses, emoji_reactions, word_responses, emoji_reactions_phrases
+    await react_configured_cases(lower_message, message, words)
 
+    # Special cases
+    await react_special_cases(lower_message, message, words)
+
+
+async def check_manual_commands(message):
+    if pattern := re.match("-say (.*)", message.content):
+        await message.delete()
+        await message.channel.send(pattern.group(1))
+    if pattern := re.match("-react (.*)", message.content):
+        await message.delete()
+        await add_reactions(message.reference.resolved, pattern.group(1).replace(' ', ''))
+    return
+
+
+async def bot_only_reactions(message):
+    if "you have been unsubscribed due to inactivity!" in message.content:
+        await message.add_reaction("😢")
+    return
+
+
+async def react_configured_cases(lower_message, message, words):
+    global phrase_responses, emoji_reactions, word_responses, emoji_reactions_phrases
     # Phrase responses
     for key in phrase_responses:
         if key in lower_message:
             await message.reply(phrase_responses[key])
-
     # word responses
     for word in words:
         if word in word_responses:
             await message.reply(word_responses[word])
         if word in emoji_reactions:
             await add_reactions(message, emoji_reactions[word])
-
     # emoji reactions
     for key in emoji_reactions_phrases:
         if key in lower_message:
             await add_reactions(message, emoji_reactions_phrases[key])
             # await message.add_reaction(emoji_reactions[key])
 
-    # Special cases
+
+async def react_special_cases(lower_message, message, words):
     if ("help" in words or "helpen" in words or "fix" in message.content) and "?" in message.content:
         await message.reply("Have you tried turning it off and on again?")
     if pattern := re.match("[iI]k ben (.*)", message.content):
---
+++ config.py
@@ -0,0 +1,48 @@
+linux_copy_pasta = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."
+
+# Non case sensitive, keys must be words
+responses = {"slapen": "Slaapwel!",
+             "kat": "Miauw",
+             "hond": "Woef",
+             "haan": "Kukeleku",
+             "vis": "Blub",
+             "linux": linux_copy_pasta,
+             "slok of gene slok": "WINOK",
+             "water": "WATER?! Wil je me soms vergiftigen ofzo?",
+             "anne-marie": "*Anne Marie",
+             }
+# Multiple keys with same value
+responses.update(dict.fromkeys(
+    ['eten', 'lunchen', 'dinner', 'voedsel', 'lunch', 'ontbijt', 'breakfast', 'brunch', 'snack', 'snacken', 'eeten', 'ete'],
+    "Smakelijk!"))
+responses.update(dict.fromkeys(['vos', 'fox'], "Ring-ding-ding-ding-dingeringeding!"))
+responses.update(dict.fromkeys(['aescu', 'aesculapia'], "ieuw"))
+
+emojis = {"p leave": "👋",
+          "good bot": "😘",
+          "tea": "🍵",
+          "cupcake": "🧁",
+          "lockdown": "🔒⬇",
+          "se": "🤮",
+          'bollekebot': "👀",
+          "weyts": "💩",
+          "bolleke": "🟠",
+          "sad": "😢",
+          "cara": "🍻",
+          "anne marie": "❤",
+          "dorien": "🍻",
+          "gitte": "💜",
+          "alexander": "🥴",
+          "f": "🇫",
+          "nice": "♋"
+          }
+
+emojis.update(dict.fromkeys(['bad bot', 'kutbot'], "😠"))
+emojis.update(dict.fromkeys(['pussy', '50 shades'], "😏"))
+emojis.update(dict.fromkeys(['birthday', "verjaardag"], "🥳"))
+emojis.update(dict.fromkeys(['love', 'liefde', 'hou van'], "😍"))
+emojis.update(dict.fromkeys(['dead', 'dood'], "🔫"))
+emojis.update(dict.fromkeys(['corona', 'covid'], "🤧"))
+emojis.update(dict.fromkeys(['king', 'mking', 'deceuninck', 'koning'], "👑"))
+emojis.update(dict.fromkeys(['sneeuw', 'sneeuwman', 'sneeuwt', 'sneeuwen'], "☃"))
+emojis.update(dict.fromkeys(["succes", "success"], "❤"))
