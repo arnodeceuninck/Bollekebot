e9448c8f8223e023f9c9ce3caaaa48025246602a
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2022-01-14 09:28:46 +0100

    Using cute pictures from unsplash

--- text_parser.py
+++ text_parser.py
@@ -3,13 +3,18 @@ import re
 
 all_matches = ""
 
-file = open("input.txt", encoding="utf8")
+file = open("cute2.html", encoding="utf8")
+
+
 regex = re.compile("(.*)url\((.*)\)(.*)")
+regex = re.compile("(.*)srcset=\"https://images\.unsplash\.com/photo-(.*)\?ixlib=rb-1\.2\.1(.*)")
+
 for line in file:
     match = regex.match(line)
     if match:
         line = match.group(2)
-        all_matches += f"{line}\n"
+        line = line.split("?")[0]
+        all_matches += f"https://images.unsplash.com/photo-{line}\n"
 
-with open('cute.txt', 'w+') as f:
+with open('output.txt', 'w+') as f:
     f.write(all_matches)
