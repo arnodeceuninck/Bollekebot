e51f028ca4fd5f211ddba467f981e8bc38d32335
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2022-01-21 11:34:54 +0100

    Quotes van Catoke

--- text_parser.py
+++ text_parser.py
@@ -8,13 +8,17 @@ file = open("cute2.html", encoding="utf8")
 
 regex = re.compile("(.*)url\((.*)\)(.*)")
 regex = re.compile("(.*)srcset=\"https://images\.unsplash\.com/photo-(.*)\?ixlib=rb-1\.2\.1(.*)")
+regex = re.compile("[0-9]+\. (.*)")
 
 for line in file:
     match = regex.match(line)
     if match:
-        line = match.group(2)
+        line = match.group(1)
         line = line.split("?")[0]
-        all_matches += f"https://images.unsplash.com/photo-{line}\n"
+        # all_matches += f"https://images.unsplash.com/photo-{line}\n"
+        all_matches += f"{line}\n"
+    else:
+        print(f"Skipping {line}")
 
 with open('output.txt', 'w+') as f:
     f.write(all_matches)
