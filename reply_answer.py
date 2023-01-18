7bd18e41e9bb87760d0ba810c0f6b0ee556776a9
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2023-01-18 21:10:15 +0100

    attempt to fix unclosed file

--- reply_answer.py
+++ reply_answer.py
@@ -7,9 +7,6 @@ class ReplyAnswer(Answer):
     def __init__(self, reply_text, as_reply=True, image=None, **kwargs):
         super().__init__(**kwargs)
 
-        if image is not None:
-            image = discord.File(image)
-
         self.reply_text = reply_text  # Should be either a string or a function returning a string
         self.as_reply = as_reply
         self.image = image
@@ -20,17 +17,22 @@ class ReplyAnswer(Answer):
         else:
             return self.reply_text(message)
 
+    async def _send(self, message, text, kwargs):
+        if self.as_reply:
+            await message.get_discord_message().reply(text, **kwargs)
+        else:
+            await message.get_discord_message().channel.send(text, **kwargs)
+
     async def send_answer(self, message, client):
         text = self.get_text(message)
 
         kwargs = {}
 
         if self.image is not None:
-            kwargs["file"] = self.image
+            kwargs["file"] = discord.File(self.image)
 
-        if self.as_reply:
-            await message.get_discord_message().reply(text, **kwargs)
-        else:
-            await message.get_discord_message().channel.send(text, **kwargs)
+        await self._send(message, text, kwargs)
+
+
 
 
